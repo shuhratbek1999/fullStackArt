57481766d02c4173e4abb007cb3c508b33f0579d
shuhratbek1999 <shuhratbekzokirjonov1119@gmail.com>
2024-02-13 20:54:37 +0500

    user

--- src/main.js
+++ src/main.js
@@ -27,15 +27,15 @@ const Bearer = "Bearer ";
 let BASE_URL = "http://164.90.232.255:3000/api/v1/admin-app/";
 // let BASE_URL = "http://localhost:3010/api/v1/admin-app/";
 let FILE_URL = BASE_URL
-axios.interceptors.request.use(
-    function (config) {
-      config.headers["Authorization"] = Bearer + localStorage.getItem("token");
-      return config;
-    },
-    function (error) {
-      return Promise.reject(error);
-    }
-  )
+// axios.interceptors.request.use(
+//     function (config) {
+//       config.headers["Authorization"] = Bearer + localStorage.getItem("token");
+//       return config;
+//     },
+//     function (error) {
+//       return Promise.reject(error);
+//     }
+//   )
 import './css/style.css'
 axios.defaults.baseURL = BASE_URL;
 app.provide('FILE_URL', FILE_URL)
